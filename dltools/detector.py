ef61dfd0a3440b194d835958759b4e54eb2eed3a
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2019-03-28 15:31:22 +0100

    Add lr as argument

--- dltools/detector.py
+++ dltools/detector.py
@@ -39,9 +39,10 @@ class ObjectDetector(object):
 
     """
 
-    def __init__(self, model, batch_size=32, epoch=2, model_check_point=True):
+    def __init__(self, model, learning_rate=1e-4, batch_size=32, epoch=2, model_check_point=True):
         self.model_ = self._build_model(model)
-        self.params_model_ = self._init_params_model()
+        self.params_model = self._init_params_model()
+        self.params_model.lr = learning_rate
         self.batch_size = batch_size
         self.epoch = epoch
         self.model_check_point = model_check_point
@@ -125,25 +126,6 @@ class ObjectDetector(object):
     def _init_params_model():
         params_model = Bunch()
 
-        # image and class parameters
-        params_model.img_rows = 128
-        params_model.img_cols = 128
-        params_model.img_channels = 1
-
-        # architecture params
-        params_model.output_channels = 1            # size of the output in depth
-        params_model.depth = 16                     # depth of all hidden layers
-        params_model.n_layers = 6                   # number of layers before last
-        params_model.conv_size0 = (3, 3)            # kernel size of first layer
-        params_model.conv_size = (3, 3)             # kernel size of intermediate layers
-        params_model.last_conv_size = (3, 3)        # kernel size of last layer
-        params_model.activation = 'relu'            # activation between layers
-        params_model.last_activation = 'sigmoid'    # final activation (sigmoid nice if binary objective)
-        params_model.initialization = 'he_normal'   # weight initialization
-        params_model.constraint = None              # kernel constraints (None, nonneg, unitnorm, maxnorm)
-        params_model.dropout_rate = 0.0             # percentage of weights not updated (0 = no dropout)
-        params_model.sigma_noise = 0.01             # random noise added before last layer (0 = no noise added)
-
         # optimizer parameters
         params_model.lr = 1e-4
         params_model.beta_1 = 0.9
@@ -159,8 +141,6 @@ class ObjectDetector(object):
         params_model.es_patience = 12
         params_model.es_min_delta = 0.001
 
-        
-
         params_model.reduce_learning_rate = True
         params_model.lr_patience = 5
         params_model.lr_factor = 0.5
@@ -174,9 +154,7 @@ class ObjectDetector(object):
     def _build_model(self, model):
 
         # load the parameter for the SSD model
-        params_model = self._init_params_model()
-
-        optimizer = Adam(lr=params_model.lr)
+        optimizer = Adam(lr=self.params_model_.lr)
 
         model.compile(optimizer=optimizer, loss=params_model.keras_loss)
 
